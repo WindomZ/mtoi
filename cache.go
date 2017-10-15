c40664226b0ea83ac186f187e86b3fee54a85982
WindomZ <git.windomz@gmail.com>
2017-10-15 15:46:53 +0800

    feat: cache impl & testing

---
+++ cache.go
@@ -0,0 +1,117 @@
+package mtoi
+
+import (
+	"sync"
+	"time"
+)
+
+type itemCache struct {
+	Key        string
+	Value      interface{}
+	ExpireTime int64
+}
+
+type Cache struct {
+	cap       int
+	interval  time.Duration
+	data      map[string]*itemCache
+	stream    chan *itemCache
+	interrupt chan bool
+	lock      *sync.RWMutex
+}
+
+func NewCache(cap int, interval time.Duration) *Cache {
+	if cap <= 2 {
+		cap = 2
+	}
+	if interval <= 0 {
+		interval = time.Minute * 5
+	}
+	c := &Cache{
+		cap:       cap,
+		interval:  interval,
+		data:      make(map[string]*itemCache, cap),
+		stream:    make(chan *itemCache, cap),
+		interrupt: make(chan bool),
+		lock:      new(sync.RWMutex),
+	}
+	c.start()
+	return c
+}
+
+func (c *Cache) start() {
+	go func() {
+		for v, ok := <-c.stream; ok; v, ok = <-c.stream {
+			if v != nil {
+				c.lock.Lock()
+				for ; v != nil && ok; v, ok = <-c.stream {
+					c.data[v.Key] = v
+					if len(c.data) > c.cap {
+						c.cap = len(c.data) + 2
+					}
+				}
+				c.lock.Unlock()
+			}
+		}
+	}()
+	go func() {
+		for ok := true; ok; {
+			select {
+			case <-c.interrupt:
+				ok = false
+			case <-time.After(c.interval):
+				c.lock.Lock()
+				now := time.Now().Unix()
+				for _, v := range c.data {
+					if v.ExpireTime < now {
+						delete(c.data, v.Key)
+					}
+				}
+				c.lock.Unlock()
+			}
+		}
+	}()
+}
+
+func (c *Cache) Close() {
+	close(c.stream)
+	c.interrupt <- true
+	close(c.interrupt)
+}
+
+func (c Cache) Size() int {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return len(c.data)
+}
+
+func (c *Cache) Clean() {
+	c.lock.Lock()
+	c.data = make(map[string]*itemCache, c.cap)
+	c.lock.Unlock()
+}
+
+func (c *Cache) Put(k string, v interface{}, expire time.Duration) {
+	if k != "" && expire > 0 {
+		c.stream <- &itemCache{k, v,
+			time.Now().Add(expire).Unix()}
+		c.stream <- nil
+	}
+}
+
+func (c Cache) Get(k string) (interface{}, bool) {
+	c.lock.RLock()
+	v, ok := c.data[k]
+	c.lock.RUnlock()
+	if ok && v.ExpireTime >= time.Now().Unix() {
+		return v.Value, true
+	}
+	return nil, false
+}
+
+func (c Cache) Contain(k string) bool {
+	c.lock.RLock()
+	_, ok := c.data[k]
+	c.lock.RUnlock()
+	return ok
+}
---
+++ cache_test.go
@@ -0,0 +1,73 @@
+package mtoi
+
+import (
+	"testing"
+	"time"
+
+	"github.com/WindomZ/testify/assert"
+)
+
+var cache *Cache
+
+func TestCache_NewCache(t *testing.T) {
+	cache = NewCache(2, time.Second)
+}
+
+func TestCache_Put(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		s := string(demo[i])
+		cache.Put(s, s, time.Second)
+	}
+}
+
+func TestCache_Size(t *testing.T) {
+	assert.Equal(t, 10, cache.Size())
+}
+
+func TestCache_Get(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		k := string(demo[i])
+		v, ok := cache.Get(k)
+		if assert.True(t, ok) {
+			s, ok := v.(string)
+			assert.True(t, ok)
+			assert.NotEmpty(t, s)
+			assert.Equal(t, 1, len(s))
+		}
+	}
+}
+
+func TestCache_Contain(t *testing.T) {
+	assert.True(t, cache.Contain("a"))
+	assert.False(t, cache.Contain("z"))
+}
+
+func TestCache_Size2(t *testing.T) {
+	assert.Equal(t, 10, cache.Size())
+}
+
+func TestCache_Get2(t *testing.T) {
+	time.Sleep(time.Second * 2)
+	for i := 0; i < 10; i++ {
+		k := string(demo[i])
+		_, ok := cache.Get(k)
+		assert.False(t, ok)
+	}
+}
+
+func TestCache_Size3(t *testing.T) {
+	time.Sleep(time.Second)
+	assert.Equal(t, 0, cache.Size())
+}
+
+func TestCache_Clean(t *testing.T) {
+	cache.Clean()
+}
+
+func TestCache_Size4(t *testing.T) {
+	assert.Equal(t, 0, cache.Size())
+}
+
+func TestCache_Close(t *testing.T) {
+	cache.Close()
+}
